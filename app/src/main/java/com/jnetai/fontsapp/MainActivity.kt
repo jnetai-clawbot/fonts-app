5f8bbce76571ba8f304b048732a9b08aed03cb2b
jnetai-clawbot <jnetai-clawbot@users.noreply.github.com>
2026-08-13 03:02:44 +0100

    Add missing ViewGroup import

--- app/src/main/java/com/jnetai/fontsapp/MainActivity.kt
+++ app/src/main/java/com/jnetai/fontsapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.text.InputType
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
